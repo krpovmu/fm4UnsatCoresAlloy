61f66180242886aac957533400b0d306ee34a73a
krpovmu <krpovmu@gmail.com>
2023-12-05 16:02:30 +0100

    Sigs and Predicates to evaluate ddmin add regex

--- src/main/java/de/buw/fm4se/PredSigMUSCores.java
+++ src/main/java/de/buw/fm4se/PredSigMUSCores.java
@@ -2,6 +2,8 @@ package de.buw.fm4se;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import edu.mit.csail.sdg.alloy4.A4Reporter;
 import edu.mit.csail.sdg.alloy4.Err;
@@ -15,14 +17,23 @@ import edu.mit.csail.sdg.translator.A4Options;
 public class PredSigMUSCores<T, E> extends AbstractDdminMUS<T, E> {
 	public List<Expr> findMUS(String fileName, A4Options options, A4Reporter reporter, int printOption) {
 		List<Expr> listPredSigs = new ArrayList<Expr>();
+		String regex = "\\brun\\w*\\$";
+		Pattern pattern = Pattern.compile(regex);
 		CompModule module = CompUtil.parseEverything_fromFile(null, null, fileName);
 		try {
-			for (Func predator : module.getAllFunc()) {
-				System.out.println(predator.label + " ->  " + predator.getBody());
-			}
 			for (Sig predName : module.getAllSigs()) {
 				System.out.println("Signature: " + predName);
 			}
+			for (Func predator : module.getAllFunc()) {
+				System.out.println(predator.label + " ->  " + predator.getBody());
+				Matcher match = pattern.matcher(predator.label);
+						
+				if (match.find()) {
+
+				}
+
+			}
+
 		} catch (Err e) {
 			e.printStackTrace();
 		}
